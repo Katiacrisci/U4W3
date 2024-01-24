334c732bfcf215d849e0da2ca2334aa4e2212b5f
Katiacrisci <katcrisci@gmail.com>
2024-01-24 23:16:20 +0100

    Esercizio giorno 2

---
+++ src/day2/gestioneEventi/main/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package day2.gestioneEventi.main.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
